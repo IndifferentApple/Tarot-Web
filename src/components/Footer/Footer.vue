32d56b301d4a72879e759205fc160ecd1dc9098f
Peter Hu <30270919@qq.com>
2025-03-27 11:13:57 +0800

    Update Footer.vue
    名称

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="w-full max-w-[888px] px-[16.66px] mt-8 pb-8">
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-6 md:gap-16 w-full max-w-[1666px] border-t">
       <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400">
-        <p>塔罗牌占卜 由 <a href="https://www.vvhan.com" target="_blank" title="韩小韩博客">韩小韩博客</a> 提供技术支持</p>
+        <p>塔罗牌占卜 由 <a href="https://www.vvhan.com" target="_blank" title="弈秋博客">弈秋博客</a> 提供技术支持</p>
         <p>开源地址 <a href="https://github.com/uxiaohan/Tarot-Web" target="_blank" title="Tarot-Web">Tarot-Web</a></p>
       </section>
       <section class="flex flex-col items-center gap-2">
@@ -13,7 +13,7 @@
           <a title="Github" target="_blank" rel="nofollow" href="https://github.com/uxiaohan">
             <img class="w-8" src="@/assets/images/footer/github.svg" />
           </a>
-          <a title="韩小韩博客" target="_blank" href="https://www.vvhan.com/">
+          <a title="弈秋博客" target="_blank" href="https://www.vvhan.com/">
             <img class="w-8" src="@/assets/images/footer/blog.svg" />
           </a>
         </div>
